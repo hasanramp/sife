6c4fb7872162ecaf90b472b95eb9221c077c423b
hasan <rampurawalahasan4@gmail.com>
2021-09-07 19:06:08 +0530

    added more files to ignore in .gitignore

--- test.py
+++
@@ -1,4 +0,0 @@
-from sql_handler import sql_handler
-
-sqh = sql_handler('hasan', 'Iloveubuntu1!', 'passwords')
-print(sqh.execute('SELECT password FROM passwords WHERE website = "amazon" AND username = "hasan"')[0][0])
